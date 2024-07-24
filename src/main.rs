ddad00f1cdd6e3bbdad380862dd1badb3dbe4b2a
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-07-23 22:09:18 -0400

    Hold button after loop

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,12 @@ use panic_halt as _;
 
 use cortex_m_rt::entry;
 use embedded_hal::delay::DelayNs;
+use embedded_hal::digital::InputPin;
 use microbit::{board::Board, display::blocking::Display, hal::Timer};
 
 #[entry]
 fn main() -> ! {
-    if let Some(board) = Board::take() {
+    if let Some(mut board) = Board::take() {
         let mut timer = Timer::new(board.TIMER0);
         let mut display = Display::new(board.display_pins);
 
@@ -67,14 +68,26 @@ fn main() -> ! {
             [0, 0, 1, 0, 0],
         ];
         loop {
-            display.show(&mut timer, letter_I, 1000);
-            display.show(&mut timer, heart, 1000);
-            display.show(&mut timer, letter_R, 1000);
-            display.show(&mut timer, letter_u, 1000);
-            display.show(&mut timer, letter_s, 1000);
-            display.show(&mut timer, letter_t, 1000);
-            display.clear();
-            timer.delay_ms(250_u32);
+            if let Ok(true) = board.buttons.button_a.is_low() {
+                display.show(&mut timer, [
+                    [1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1],
+                    [1, 1, 1, 1, 1],
+                ], 1000);
+                display.clear();
+                timer.delay_ms(250_u32);
+            } else {
+                display.show(&mut timer, letter_I, 1000);
+                display.show(&mut timer, heart, 1000);
+                display.show(&mut timer, letter_R, 1000);
+                display.show(&mut timer, letter_u, 1000);
+                display.show(&mut timer, letter_s, 1000);
+                display.show(&mut timer, letter_t, 1000);
+                display.clear();
+                timer.delay_ms(250_u32);
+            }
         }
     }
 
