5277209262a811f29aa9b7959c82bd0cbcc498b3
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-07-25 16:27:38 -0400

    New Lights by Marimer

--- src/main.rs
+++ src/main.rs
@@ -17,19 +17,19 @@ fn main() -> ! {
 
         #[allow(non_snake_case)]
         let letter_I = [
-            [0, 1, 1, 1, 0],
             [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 1, 1, 1, 0],
+            [0, 1, 0, 1, 0],
+            [1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 1],
+            [1, 0, 0, 0, 1],
         ];
 
         let heart = [
+            [0, 1, 1, 0, 0],
             [0, 1, 0, 1, 0],
-            [1, 0, 1, 0, 1],
-            [1, 0, 0, 0, 1],
+            [0, 1, 1, 0, 0],
             [0, 1, 0, 1, 0],
-            [0, 0, 1, 0, 0],
+            [0, 1, 1, 0, 0],
         ];
 
         #[allow(non_snake_case)]
@@ -43,29 +43,29 @@ fn main() -> ! {
 
         #[allow(non_snake_case)]
         let letter_u = [
-            [1, 0, 0, 0, 1],
-            [1, 0, 0, 0, 1],
-            [1, 0, 0, 0, 1],
-            [1, 0, 0, 0, 1],
+            [0, 1, 1, 1, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0],
             [0, 1, 1, 1, 0],
         ];
 
         #[allow(non_snake_case)]
         let letter_s = [
-            [0, 1, 1, 1, 0],
             [1, 0, 0, 0, 0],
-            [0, 1, 1, 1, 0],
-            [0, 0, 0, 0, 1],
-            [0, 1, 1, 1, 0],
+            [1, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0],
+            [1, 1, 1, 1, 0],
         ];
 
         #[allow(non_snake_case)]
         let letter_t = [
             [1, 1, 1, 1, 1],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
-            [0, 0, 1, 0, 0],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
         ];
         loop {
             if let Ok(true) = board.buttons.button_a.is_low() {
@@ -78,7 +78,19 @@ fn main() -> ! {
                 ], 1000);
                 display.clear();
                 timer.delay_ms(250_u32);
-            } else {
+            }
+                else if let Ok(true) = board.buttons.button_b.is_low()  {
+                    display.show(&mut timer, [
+                        [0, 1, 0, 1, 0],
+                        [1, 0, 1, 0, 1],
+                        [1, 0, 1, 0, 1],
+                        [1, 0, 0, 0, 1],
+                        [1, 0, 0, 0, 1],
+                    ], 1000 * 3);
+                    display.clear();
+                    timer.delay_ms(250_u32);
+                }
+            else {
                 display.show(&mut timer, letter_I, 1000);
                 display.show(&mut timer, heart, 1000);
                 display.show(&mut timer, letter_R, 1000);
